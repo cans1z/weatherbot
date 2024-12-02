66e21bc05bd92996892c5f07e08340c02c5c1b34
cans1z <canss1zzz@gmail.com>
2024-12-02 09:29:03 +0300

    хуй

--- Host.cs
+++ Host.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System.Threading;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
+using Quartz;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace weatherbot
@@ -9,16 +11,28 @@ namespace weatherbot
     internal class Host
     {
 
+        Stack<Chat> chats = new Stack<Chat>();
         TelegramBotClient _bot;
+        private TimerCallback _timerCallback;
+        private Timer _scheduleTimer;
+
         public Host(string token)
         {
             _bot = new TelegramBotClient(token);
+            _timerCallback = new TimerCallback(InvokeCallback);
+            _scheduleTimer = new Timer(_timerCallback, 52, 0, 1000);
+        }
+
+        public void InvokeCallback(object obj)
+        {
+
         }
 
         public void Start()
         {
             _bot.StartReceiving(UpdateHandler, ErrorHandler);
             Console.WriteLine("bot get started");
+            
         }
 
         private Task ErrorHandler(ITelegramBotClient client, Exception exception,
@@ -30,13 +44,40 @@ namespace weatherbot
 
         private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken cancellation)
         {
+            if (update.Message?.Text == "/start")
+            {
+                chats.Push(update.Message.Chat);
+            }
+            if (update.Message?.Text == "/send")
+            {
+                foreach (Chat chat in chats) 
+                {
+                    _bot.SendMessage(chat, "gay");
+                }
+            }
             Console.WriteLine($"got message: {update.Message?.Text ?? "[not text]"}");
             //return client.SendTextMessageAsync(update.Message?.Chat, "неформал");
-            //await _bot.SendMessage(chatId, "Welcome! Pick one direction");
-            ChatId chatId = 7330841099;
-            await _bot.SendMessage(chatId, "Trying all the parameters of sendMessage method");
+            //ChatId chatId = 7330841099;
+            await _bot.SendMessage(update.Message.Chat, "bot is active");
 
         }
+
+        private async Task SendMessage()
+        {
+            foreach (Chat chat in chats)
+            {
+                _bot.SendMessage(chat, "gay");
+            }
+        }
+
+
+
+        //public void StartScheduling(DateTime scheduledTime)
+        //{   
+
+        //}
+
+
 
 
     }
