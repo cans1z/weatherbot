faa8341a551cbf2948c615ba6cb51d77de616378
cans1z <canss1zzz@gmail.com>
2025-04-22 19:09:38 +0300

    changing user's time via bot's settings

--- Management.cs
+++ Management.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Microsoft.VisualBasic;
 using System;
 using System.Threading;
+using System.Timers;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
@@ -12,6 +14,7 @@ using weatherbot.Models;
 using weatherbot.Providers;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 using static System.Net.Mime.MediaTypeNames;
+using Timer = System.Threading.Timer;
 using Update = Telegram.Bot.Types.Update;
 
 namespace weatherbot
@@ -26,10 +29,9 @@ namespace weatherbot
 
         public Management(string token)
         {
-                _bot = new TelegramBotClient(token);
+            _bot = new TelegramBotClient(token);
             _timerCallback = new TimerCallback(InvokeCallback);
-            _scheduleTimer = new Timer(_timerCallback, 52, 0, 7000);
-            //weatherAPI = new WeatherAPI(uri);
+            _scheduleTimer = new Timer(_timerCallback, 52, 0, 7000); 
         }
 
         public void InvokeCallback(object obj)
@@ -54,14 +56,13 @@ namespace weatherbot
 
         public async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken cancellation)
         {
-
-            if (update.Message?.Text == "/start")
+            var user = UserProvider.GetUser(update.Message.Chat.Id  );
+            /*if (update.Message?.Text == "/start")
             {
                 UserProvider.AddUser(new Models.User { TgId = update.Message.Chat.Id });
                 TimesProvider.AddTime(new Time { UserId = UserProvider.GetUser(update.Message.Chat.Id).Id });
             }
 
-            var user = UserProvider.GetUser(update.Message.Chat.Id);
             if (user != null && user.State == "change city")
             {
                 var city = update.Message?.Text;
@@ -80,7 +81,72 @@ namespace weatherbot
                 await _bot.SendMessage(update.Message.Chat.Id, "Введите время для отправки сообщений:");
                 UserProvider.ChangeState(update.Message.Chat.Id, "change time");
                 return;
+            }*/
+
+            switch (update.Message?.Text)
+            {
+                case "/start":
+                    UserProvider.AddUser(new Models.User { TgId = update.Message.Chat.Id});
+                    TimesProvider.AddTime(new Time { UserId = UserProvider.GetUser(update.Message.Chat.Id).Id});
+                    break;
+
+                case "/change city":
+                    await _bot.SendMessage(update.Message.Chat.Id, "Введите название города:");
+                    UserProvider.ChangeState(update.Message.Chat.Id, "change city");
+                    return;
+
+                case "/change time":
+                    var times = TimesProvider.Times(user.Id).Select(item => item.TimeStr);
+                    string answer = "";
+                    foreach (string time in times)
+                    {
+                        answer = answer + time + "\n";
+                    }
+                    await _bot.SendMessage(update.Message.Chat.Id, "Доступное время: " + "\n" + answer + "\n" +
+                    "Выберите действие:" + "\n" + "1. Добавить время" + "\n" + "2. Удалить время");
+                    UserProvider.ChangeState(update.Message.Chat.Id, "change time");
+                    return;
             }
+            if (user != null && user.State == "change city")
+            {
+                var city = update.Message?.Text;
+                UserProvider.ChangeCity(update.Message.Chat.Id, city);
+            }
+
+            if (user != null && user.State == "change time")
+            {
+                if (update.Message.Text == "1")
+                {
+                    await _bot.SendMessage(update.Message.Chat.Id, "Введите время для отправки сообщений");
+                    UserProvider.ChangeState(update.Message.Chat.Id, "add time");
+                }
+                else if (update.Message.Text == "2")
+                {
+                    await _bot.SendMessage(update.Message.Chat.Id, "Введите время, которое необходимо удалить");
+                    UserProvider.ChangeState(update.Message.Chat.Id, "delete time");
+                }
+                else
+                {
+                    await _bot.SendMessage(update.Message.Chat.Id, "Введен некорректный формат для выполнения команды");
+                    UserProvider.ChangeState(update.Message.Chat.Id, "default");
+                }
+            }
+
+            if (user.State == "add time")
+            {
+                string time = update.Message.Text;
+                TimesProvider.AddTime(new Time { UserId = UserProvider.GetUser(update.Message.Chat.Id).Id, TimeStr = time });
+                UserProvider.ChangeState(update.Message.Chat.Id, "default");
+            }
+            
+            if (user.State == "delete time")
+            {
+                string timestr = update.Message.Text;
+                var time = TimesProvider.GetTime(timestr);
+                TimesProvider.DeleteTime(time);
+                UserProvider.ChangeState(update.Message.Chat.Id, "default");
+            }
+
         }
 
 
--- Providers/TimesProvider.cs
+++ Providers/TimesProvider.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot.Types;
 using weatherbot.Models;
 
 
@@ -18,6 +22,14 @@ namespace weatherbot.Providers
             }
         }
 
+        public static Time GetTime(string time)
+        {
+            using (var db = new ApplicationContext())
+            {
+                return db.Times.Where(item => item.TimeStr == time).FirstOrDefault();
+            }
+        }
+
         public static void AddTime(Time time)
         {
             using (var db = new ApplicationContext())
@@ -27,7 +39,16 @@ namespace weatherbot.Providers
             }
         }
 
-        public static List<Time> ListAllTimes()
+        public static void DeleteTime(Time time)
+        {
+            using (var db = new ApplicationContext())
+            {
+                db.Times.Remove(time);
+                db.SaveChanges();
+            }
+        }
+
+        public static List<Time> ListAllTimes() 
         {
             using (var db = new ApplicationContext())
             {
